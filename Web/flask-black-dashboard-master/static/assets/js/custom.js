e257768df6f51ead58ad4de0783b55c28adcc26f
Jadson De Souza Maciel <jadsonm90@gmail.com>
2025-12-16 16:10:52 -0400

    Acrescimo de codigo de site, com kpis, grafico de linha e gauge.

---
+++ Web/flask-black-dashboard-master/static/assets/js/custom.js
@@ -0,0 +1,299 @@
+
+var gradientChartOptionsConfigurationWithTooltipmain = {
+      maintainAspectRatio: false,
+
+      scales: { 
+        yAxes: [{
+            id: 'y-axis-0',
+            display: true,
+            ticks: {
+                min: 200,
+                max: 500,
+                stepSize: 25,
+            },
+        }]
+    
+        },
+
+
+      legend: { display: false },
+
+      tooltips: {
+        backgroundColor: '#f5f5f5',
+        titleFontColor: '#333',
+        bodyFontColor: '#666',
+        bodySpacing: 4,
+        xPadding: 12,
+        mode: "nearest",
+        intersect: 0,
+        position: "nearest"
+      },
+      responsive: true,
+
+      annotation: {
+        annotations: [{
+            type: 'line',
+            mode: 'horizontal',
+            scaleID: 'y-axis-0',
+            value: 350,
+            borderColor: '#4dff50ff',
+            borderWidth: 1,
+            label: {
+                enabled: true,
+                content: 'Limite seguro',
+                position: "top",  
+                yAdjust: -10, 
+                backgroundColor: "rgba(0, 0, 0, 0.2)"
+                
+            }
+        }]
+    },
+
+    };
+
+
+    var gradientChartOptionsConfigurationWithTooltipco2 = {
+      maintainAspectRatio: false,
+
+      scales: { 
+        yAxes: [{
+            id: 'y-axis-0',
+            display: true,
+            ticks: {
+                min: 300,
+                max: 500,
+                stepSize: 25,
+            },
+        }]
+    
+        },
+
+
+      legend: { display: false },
+
+      tooltips: {
+        backgroundColor: '#f5f5f5',
+        titleFontColor: '#333',
+        bodyFontColor: '#666',
+        bodySpacing: 4,
+        xPadding: 12,
+        mode: "nearest",
+        intersect: 0,
+        position: "nearest"
+      },
+      responsive: true,
+
+
+    };
+
+     var gradientChartOptionsConfigurationWithTooltippH = {
+      maintainAspectRatio: false,
+
+      scales: { 
+        yAxes: [{
+            id: 'y-axis-0',
+            display: true,
+            ticks: {
+                min: 0,
+                max: 14,
+                stepSize: 1,
+            },
+        }]
+    
+        },
+
+
+      legend: { display: false },
+
+      tooltips: {
+        backgroundColor: '#f5f5f5',
+        titleFontColor: '#333',
+        bodyFontColor: '#666',
+        bodySpacing: 4,
+        xPadding: 12,
+        mode: "nearest",
+        intersect: 0,
+        position: "nearest"
+      },
+      responsive: true,
+
+
+    };
+
+     var gradientChartOptionsConfigurationWithTooltipTemperature = {
+      maintainAspectRatio: false,
+
+      scales: { 
+        yAxes: [{
+            id: 'y-axis-0',
+            display: true,
+            ticks: {
+                min: 200,
+                max: 500,
+                stepSize: 25,
+            },
+        }]
+    
+        },
+
+
+      legend: { display: false },
+
+      tooltips: {
+        backgroundColor: '#f5f5f5',
+        titleFontColor: '#333',
+        bodyFontColor: '#666',
+        bodySpacing: 4,
+        xPadding: 12,
+        mode: "nearest",
+        intersect: 0,
+        position: "nearest"
+      },
+      responsive: true,
+
+
+    };
+
+     var gradientChartOptionsConfigurationWithTooltipLuminosity = {
+      maintainAspectRatio: false,
+
+      scales: { 
+        yAxes: [{
+            id: 'y-axis-0',
+            display: true,
+            ticks: {
+                min: 200,
+                max: 500,
+                stepSize: 25,
+            },
+        }]
+    
+        },
+
+
+      legend: { display: false },
+
+      tooltips: {
+        backgroundColor: '#f5f5f5',
+        titleFontColor: '#333',
+        bodyFontColor: '#666',
+        bodySpacing: 4,
+        xPadding: 12,
+        mode: "nearest",
+        intersect: 0,
+        position: "nearest"
+      },
+      responsive: true,
+
+
+    };
+
+
+
+   
+
+
+
+
+main = {
+
+
+
+    createLineChart: function(canvasId, labels, dataset_label, data, color, options, pR = 3, fill = false) {
+    var ctx = document.getElementById(canvasId).getContext('2d');
+
+    return new Chart(ctx, {
+      type: 'line',
+      data: {
+        labels: labels,
+        datasets: [{
+          label: dataset_label,
+          data: data,
+          borderColor: color,
+          pointBackgroundColor: color,
+          backgroundColor: color.replace('1)', '0.13)'),
+          borderWidth: 2,
+          pointRadius: pR,
+          fill: fill
+        }]
+      },
+      options: options
+    })
+    },
+  
+
+
+ initDashboardPageCharts: function () {
+    // dados fictícios para exemplo - eixo x
+
+    var chart_labels_Quinzenal_main = ['1','2','3','4','5','6','7','8','9','10','11','12','13','14','15'];
+    var chart_labels_5min_co2 = ['12:00','12:05','12:10','12:15','12:20','12:25','12:30','12:35','12:40','12:45','12:50','12:55'];
+    
+    // dados fictícios para exemplo - eixo y
+
+    var chart_data_Quinzenal_main = [ 425,420,416,411,407,402,398,393,389,384,379,373,368,360,350];
+    var chart_data_5min_co2 = [422,421,419,420,418,417,415,414,412,411,409,408]
+    var chart_data_5min_pH = [7,7,7,7,7,7,7,7,7,7,7,7]
+
+
+    this.createLineChart("chartBig1", chart_labels_Quinzenal_main, 'CO2', chart_data_Quinzenal_main, 'rgba(0, 214, 180, 1)', gradientChartOptionsConfigurationWithTooltipmain, 3 ,true)
+    this.createLineChart("chartCo2", chart_labels_5min_co2, 'CO2', chart_data_5min_co2, 'rgba(0, 214, 68, 1)', gradientChartOptionsConfigurationWithTooltipco2, 0)
+    // this.createLineChart("chartpH", chart_labels_5min_co2, 'pH', chart_data_5min_pH, 'rgba(214, 0, 180, 1)', gradientChartOptionsConfigurationWithTooltippH)
+    this.createLineChart("chartTemperature", chart_labels_5min_co2, 'Temperatura', chart_data_5min_co2, 'rgba(214, 180, 0, 1)', gradientChartOptionsConfigurationWithTooltipTemperature)
+    this.createLineChart("chartLuminosity", chart_labels_5min_co2, 'Luminosidade', chart_data_5min_co2, 'rgba(255, 255, 255, 1)', gradientChartOptionsConfigurationWithTooltipLuminosity)
+
+const config = {
+  "id": "gauge",
+  "value": 7.4383517856623955,
+  "min": 0,
+  "max": 14,
+  "decimals": 1,
+  "symbol": "",
+  "reverse": false,
+  
+  "relativeGaugeSize": true,
+  "gaugeWidthScale": 0.8,
+  "gaugeColor": "#1e1e2f",
+  "valueFontColor": "#777777ff",
+  "labelFontColor": "#555454ff",
+  "levelColors": [
+    "#a9d70b"
+  ],
+  "noGradient": false,
+  "showSectorColors": false,
+  "title": "",
+  "titleFontColor": "#cb1313ff",
+  "titleFontFamily": "Arial",
+  "titleFontWeight": "bold",
+  "titlePosition": "above",
+  
+  "minTxt": "",
+  "maxTxt": "",
+  "hideValue": false,
+  "hideMinMax": false,
+  "humanFriendly": false,
+  "formatNumber": false,
+  "donut": false,
+  "donutStartAngle": 90,
+  "differential": false,
+  "displayRemaining": false,
+  "pointer": false,
+  "targetLineColor": "#000000",
+  "targetLineWidth": 1.5,
+  "startAnimationTime": 700,
+  "startAnimationType": ">",
+  "refreshAnimationTime": 700,
+  "counter": false,
+  "showInnerShadow": true,
+  "shadowOpacity": 0.2,
+  "shadowSize": 5,
+  "shadowVerticalOffset": 3
+}
+
+
+    const gaugeCo2 = new JustGage(config);
+
+    gaugeCo2.refresh(7);
+
+ }
+};
